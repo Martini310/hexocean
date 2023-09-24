8fdf1792b9ea515f29f5d5d85ec0694b91c22e4d
Martini310 <martin310@wp.pl>
2023-09-24 11:54:54 +0200

    Set only thumbnail sizes to select in Tier admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
-from api.models import Tier, Size, Image, Profile, TemporaryLink
+from .models import Tier, Size, Image, Profile, TemporaryLink
+from django import forms
 
-admin.site.register(Tier)
+
 admin.site.register(Size)
 admin.site.register(Image)
 admin.site.register(Profile)
 admin.site.register(TemporaryLink)
+
+
+class TierAdminForm(forms.ModelForm):
+    class Meta:
+        model = Tier
+        fields = '__all__'
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Override the queryset for thumbnail_sizes field to display only Sizes added by admin
+        self.fields['thumbnail_sizes'].queryset = Size.objects.filter(thumbnail_size=True)
+
+class TierAdmin(admin.ModelAdmin):
+    form = TierAdminForm
+
+admin.site.register(Tier, TierAdmin)
