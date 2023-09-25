88e0eb4dc8715c6e2e90b0fff3da109ad0d79c7f
Martini310 <martin310@wp.pl>
2023-09-25 22:11:02 +0200

    Create new tests

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,11 +1,15 @@
 import pytest
 from django.contrib.auth.models import User
-from api.models import Size, Tier, Profile
+from api.models import Size, Tier, Profile, Image
+from PIL import Image as PILImage
+from io import BytesIO
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 
 @pytest.fixture
 def create_basic_user():
     payload = {
-        "username": "testuser",
+        "username": "basic_user",
         "email": "user@test.pl",
         "password": "userpassword"
     }
@@ -19,7 +23,7 @@ def create_basic_user():
 @pytest.fixture
 def create_premium_user():
     payload = {
-        "username": "testuser2",
+        "username": "premium_user",
         "email": "user2@test.pl",
         "password": "userpassword"
     }
@@ -29,3 +33,35 @@ def create_premium_user():
     profile = Profile.objects.create(user=user, tier=premium)
 
     return user
+
+@pytest.fixture
+def create_enterprise_user():
+    payload = {
+        "username": "enterprise_user",
+        "email": "user3@test.pl",
+        "password": "userpassword"
+    }
+    user = User.objects.create(**payload)
+
+    enterprise = Tier.objects.filter(name='Enterprise').get()
+    profile = Profile.objects.create(user=user, tier=enterprise)
+
+    return user
+
+@pytest.fixture
+def create_image(create_enterprise_user):
+
+    image_data = BytesIO()
+    image = PILImage.new('RGB', (200, 200), 'black')
+    image.save(image_data, format='png')
+    image_data.seek(0)
+
+    size = Size.objects.first()
+    user = create_enterprise_user
+
+    image = Image.objects.create(title='Test image',
+                                 image=SimpleUploadedFile("test.png", image_data.read(), content_type='image/png'),
+                                 size=size,
+                                 user=user)
+
+    return image
--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -4,9 +4,11 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth.models import User
-from api.models import Size, Image
+from api.models import Size, Image, TemporaryLink
 from PIL import Image as PILImage
 from io import BytesIO
+from django.urls import reverse
+
 
 client = APIClient()
 
@@ -14,8 +16,8 @@ def test_pytest_working():
     assert True == True
 
 @pytest.mark.django_db
-def test_create_image_basic_user(create_basic_user):
-    user = User.objects.first()
+def test_basic_user_create_image(create_basic_user):
+    user = User.objects.get(username='basic_user')
     
     image_data = BytesIO()
     image = PILImage.new('RGB', (1000, 1000), 'white')
@@ -23,7 +25,8 @@ def test_create_image_basic_user(create_basic_user):
     image_data.seek(0)
         
     client.force_login(user)
-    url = '/api/images/'
+
+    url = reverse('api:images')
 
     payload = {
         'title': 'Test image',
@@ -32,17 +35,20 @@ def test_create_image_basic_user(create_basic_user):
     response = client.post(url, payload, format='multipart')
     print(response.content)
     content = json.loads(response.content)
+
     assert response.status_code == status.HTTP_201_CREATED
     assert Size.objects.count() == 3 # Original and 2 thumbnails (200px and 400px)
-    assert Image.objects.count() == 2 # Original and thumbnail
+    assert Image.objects.count() == 1 # Original and thumbnail
+    assert Image.objects.filter(user=user).count() == 1
     assert content['title'] == "Test image"
     assert ('/images/thumbnail_200x200_test' in content['image_200x200']) == True
+    assert content.get('image_400X400', None) == None
     assert content.get('image', None) == None
 
 
 @pytest.mark.django_db
-def test_create_image_premium_user(create_premium_user):
-    user = User.objects.get(username='testuser2')
+def test_premium_user_create_image(create_premium_user):
+    user = User.objects.get(username='premium_user')
     
     image_data = BytesIO()
     image = PILImage.new('RGB', (1000, 1000), 'black')
@@ -50,19 +56,153 @@ def test_create_image_premium_user(create_premium_user):
     image_data.seek(0)
         
     client.force_login(user)
-    url = '/api/images/'
+
+    url = reverse('api:images')
 
     payload = {
         'title': 'Test image',
         'image': SimpleUploadedFile("test.png", image_data.read(), content_type='image/png')
     }
+
     response = client.post(url, payload, format='multipart')
     print(response.content)
     content = json.loads(response.content)
+
     assert response.status_code == status.HTTP_201_CREATED
     assert Size.objects.count() == 3 # Original and 2 thumbnails (200px and 400px)
     assert Image.objects.count() == 3 # Original and 2 thumbnails (200px and 400px)
     assert content['title'] == "Test image"
     assert ('/images/thumbnail_200x200_test' in content['image_200x200']) == True
     assert ('/images/thumbnail_400x400_test' in content['image_400x400']) == True
-    assert content.get('image', None) != None
+    assert ('images/test' in content['image']) == True
+
+
+@pytest.mark.django_db
+def test_enterprise_user_create_image(create_enterprise_user):
+    user = User.objects.get(username='enterprise_user')
+    
+    image_data = BytesIO()
+    image = PILImage.new('RGB', (1000, 1000), 'black')
+    image.save(image_data, format='png')
+    image_data.seek(0)
+        
+    client.force_login(user)
+
+    url = reverse('api:images')
+
+    payload = {
+        'title': 'Test image',
+        'image': SimpleUploadedFile("test.png", image_data.read(), content_type='image/png')
+    }
+
+    response = client.post(url, payload, format='multipart')
+    print(response.content)
+    content = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert Size.objects.count() == 3 # Original and 2 thumbnails (200px and 400px)
+    assert Image.objects.count() == 3 # Original and 2 thumbnails (200px and 400px)
+    assert content['title'] == "Test image"
+    assert ('/images/thumbnail_200x200_test' in content['image_200x200']) == True
+    assert ('/images/thumbnail_400x400_test' in content['image_400x400']) == True
+    assert ('images/test' in content['image']) == True
+
+
+@pytest.mark.django_db
+def test_basic_user_cannot_generate_link(create_basic_user):
+    user = User.objects.get(username='basic_user')
+
+    client.force_login(user)
+    url = reverse('api:generate_link')
+
+    payload = {
+        'image_id': 1,
+        'exp_time': 300
+    }
+
+    response = client.post(url, payload)
+    print(response.content)
+    content = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert content['error'] == 'This feature is not available in your plan'
+
+
+@pytest.mark.django_db
+def test_premium_user_cannot_generate_link(create_premium_user):
+    user = User.objects.get(username='premium_user')
+
+    client.force_login(user)
+    url = reverse('api:generate_link')
+
+    payload = {
+        'image_id': 1,
+        'exp_time': 300
+    }
+
+    response = client.post(url, payload)
+    print(response.content)
+    content = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert content['error'] == 'This feature is not available in your plan'
+
+
+@pytest.mark.django_db
+def test_enterprise_user_cannot_generate_link_wrong_time(create_enterprise_user):
+    user = User.objects.get(username='enterprise_user')
+
+    client.force_login(user)
+    url = reverse('api:generate_link')
+
+    payload = {
+        'image_id': 1,
+        'exp_time': 200
+    }
+
+    response = client.post(url, payload)
+    print(response.content)
+    content = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert content['error'] == 'expiration time should be between 300 and 30000'
+
+
+@pytest.mark.django_db
+def test_enterprise_user_can_generate_link(create_image):
+    user = User.objects.get(username='enterprise_user')
+    image = create_image
+    client.force_login(user)
+    url = reverse('api:generate_link')
+
+    payload = {
+        'image_id': 1,
+        'exp_time': 300
+    }
+
+    response = client.post(url, payload)
+    print(response.content)
+    content = json.loads(response.content)
+    link = TemporaryLink.objects.first()
+    assert response.status_code == status.HTTP_200_OK
+    assert content['url'] == link.url
+
+
+@pytest.mark.django_db
+def test_enterprise_user_cannot_generate_link_no_image(create_image):
+    user = User.objects.get(username='enterprise_user')
+
+    client.force_login(user)
+    url = reverse('api:generate_link')
+
+    payload = {
+        'image_id': 2,
+        'exp_time': 300
+    }
+
+    response = client.post(url, payload)
+    print(response.content)
+    content = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert content['error'] == 'Image not found'
